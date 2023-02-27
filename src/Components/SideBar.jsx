705396919aa8816806353f1e45ec822a3234f90d
Eluan-Adumo <el1harolds@gmail.com>
2023-02-27 12:19:28 +0100

    New Update

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -512,14 +512,14 @@ const MapView = ()=>{
     return(
 
         <>
+
             <div className="mapouter">
                 <div className="gmap_canvas">
                     <iframe width="100%" height="100%" id="gmap_canvas"
                             src="https://maps.google.com/maps?q=Dublin Tyrells town&t=&z=9&ie=UTF8&iwloc=&output=embed"
                             frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
-                    <Link to="https://2yu.co">2yu</Link><br />
                     {/*<style>.mapouter{position:relative;text-align:right;height:100%;width:100%;}</style>*/}
-                    <Link to="https://embedgooglemap.2yu.co/">html embed google map</Link>
+                    {/*<Link to="https://embedgooglemap.2yu.co/">html embed google map</Link>*/}
                     {/*<style>.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:100%;}</style>*/}
                 </div>
             </div>
